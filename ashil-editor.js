e26960b6263d9aa1a2259298f874e5ff5009ede0
Ashil <ashiljacobtj37@gmail.com>
2022-04-10 21:56:26 +0530

    Added Custom tag in index.html

---
+++ ashil-editor.js
@@ -0,0 +1,82 @@
+
+class AshilEditor extends HTMLElement {
+
+    connectedCallback() {
+        this.innerHTML = `<ashil-editor class="custom-tag">
+
+        <div class="container container-div">
+            <div class="row" style="float: left;">
+                <h2>Text Area</h2>
+                <textarea onkeyup="callme(this);" name="" id="" cols="80" rows="10" ></textarea>
+            </div>
+
+            <div class="row container" style="float:left">
+                <p id="totalChar" class="container-p"> </p>
+
+                <p id="totalWords" class="container-p"></p>
+
+                <p id="totalLines" class="container-p" value="0"> </p>
+
+            </div>
+
+        </div>
+
+    </ashil-editor>`
+    this.style=`.home-page-h1{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        margin-left: 100px;
+      }
+      body{
+        background-color: cadetblue;
+      }
+      ashil-editor{
+        background-color: antiquewhite;
+      }`
+
+    }
+
+    
+}
+
+customElements.define('ashil-editor',AshilEditor);
+
+
+
+function callme(field) {
+
+    var charCount = field.value.length;
+
+    document.getElementById("totalChar").innerHTML="Total Char :"+charCount;
+
+
+    var wordCount = field.value.split(' ')
+                .filter(function(n) { return n != '' })
+                .length ;
+
+    var lineCount = field.value.split('.')
+                    .filter(function(n) { return n != '' })
+                    .length ;
+
+
+    
+    if( wordCount != undefined && wordCount> 0 ){
+            console.log("Word Count : ",wordCount)
+            document.getElementById("totalWords").innerHTML="Total Words :"+wordCount;
+
+        }else{
+            document.getElementById("totalWords").innerHTML="Total Words :"+0;
+        }
+
+    if( lineCount != undefined && lineCount> 0 ){
+        console.log("Total Line : ",lineCount)
+        document.getElementById("totalLines").innerHTML="Total Lines :"+lineCount;
+    }else{
+        document.getElementById("totalLines").innerHTML="Total Lines :"+0;
+
+    }
+
+    
+    console.log(field.value)
+
+}
+
